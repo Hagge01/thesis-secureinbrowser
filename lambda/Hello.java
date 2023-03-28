c7a768e713f7fd31e265f271f652cbbc79ad89f4
Dengm4n <daniel.engman99@gmail.com>
2023-03-28 10:09:45 +0200

    lambda test

--- lambda/Hello.java
+++ lambda/Hello.java
@@ -1,15 +1,25 @@
-package example;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Hello implements RequestHandler<String, String> {
-
-    @Override
-    public String handleRequest(String input, Context context) {
-        String output = "Hello, " + input + "!";
-        context.getLogger().log(output);
-        return output;
+public class MyLambdaFunction implements RequestHandler<Object, Object> {
+    public Object handleRequest(Object input, Context context) {
+        return new Response("Hello World");
     }
 
+    public static class Response {
+        private String message;
+
+        public Response(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
 }
