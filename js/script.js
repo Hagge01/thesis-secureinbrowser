c2d4fd0582e84211e39b60bc15af8cf1ed095c05
Dengm4n <daniel.engman99@gmail.com>
2023-03-28 10:21:21 +0200

    tog bort test

--- js/script.js
+++
@@ -1,4 +0,0 @@
-fetch('https://0gmpdmznid.execute-api.eu-north-1.amazonaws.com/default/THESIS-secureinbrowser')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
