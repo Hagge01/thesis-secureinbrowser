2e3d5c34684ffca4b9a7aa0e783d97c32af9ea28
Hagman01 <wille.hagman@gmail.com>
2023-03-28 10:28:37 +0200

    testing lambda

--- lambda/test.py
+++ lambda/test.py
@@ -1,14 +1,4 @@
 
 
 def lambda_handler(event, context):
-    try:
-        import com.example
-    except ImportError as e:
-        return {
-            "errorMessage": str(e),
-            "errorType": "Runtime.ImportModuleError",
-            "requestId": context.aws_request_id,
-            "stackTrace": []
-        }
-
-    return "Hello from Lambda!"
+    return "Hello from Lambdaw!"
