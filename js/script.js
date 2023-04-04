05648ba187b0893f8f10921e481f0c42ff8f2572
Dengm4n <daniel.engman99@gmail.com>
2023-04-04 10:25:02 +0200

    testar cognitpo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import { Auth } from 'aws-amplify';
+
+// Add an event listener to the sign-out button or link
+document.getElementById('sign-out-button').addEventListener('click', () => {
+  // Call the Auth.signOut() method to sign the user out
+  Auth.signOut()
+    .then(() => {
+      // Redirect the user to a different page or display a message
+      window.location.href = '/pages/sida2.html';
+    })
+    .catch((error) => {
+      console.log('Error signing out:', error);
+    });
+});
