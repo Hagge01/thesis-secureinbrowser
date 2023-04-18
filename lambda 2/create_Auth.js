8bdd03d97967e10c4706622bd881e2e29f6d6770
Dengm4n <daniel.engman99@gmail.com>
2023-04-18 15:22:38 +0200

    test

--- lambda 2/create_Auth.js
+++
@@ -1,3 +0,0 @@
-import { Context, Callback } from 'aws-lambda';
-import { generateRegistrationOptions, generateAuthenticationOptions } from '@simplewebauthn/server';
-import { CognitoCreateAuthEvent, Authenticator } from './local-types';
