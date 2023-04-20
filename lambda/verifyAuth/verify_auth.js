c0c3f580d0bf8dbec25af00b99337691fdf1a0df
Dengm4n <daniel.engman99@gmail.com>
2023-04-20 11:17:13 +0200

    rpid change

--- lambda/verifyAuth/verify_auth.js
+++ lambda/verifyAuth/verify_auth.js
@@ -4,7 +4,7 @@ const { verifyRegistrationResponse, verifyAuthenticationResponse, VerifiedRegist
 
 var cognito = new CognitoIdentityServiceProvider();
 
-//const rpID = 'https://thesis-secureinbrowser.s3.eu-north-1.amazonaws.com/pages/index.html';
+const rpID = 'thesis-secureinbrowser.s3.eu-north-1.amazonaws.com/pages/index.html';
 const origin = 'https://thesis-secureinbrowser.s3.eu-north-1.amazonaws.com';
 // The URL at which attestations and assertions should occur
 
@@ -79,7 +79,7 @@ exports.handler = async function(event, context, callback) {
             credential: challengeAnswer,
             expectedChallenge: event.request.privateChallengeParameters.attestationChallenge,
             expectedOrigin: origin,
-            //expectedRPID: rpID,
+            expectedRPID: rpID,
         });
 
         // Can register new authenticator?
