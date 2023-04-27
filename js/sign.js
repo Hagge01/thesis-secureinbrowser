7d87da7cf732d6b539edee00bcfc56a00e5ac92f
Hagman01 <wille.hagman@gmail.com>
2023-04-27 10:33:56 +0200

    updated index

--- js/sign.js
+++ js/sign.js
@@ -1,3 +1,44 @@
+
+document.getElementById('authUsername').addEventListener('click', async () => {
+    userPool = await getAmazonCognitoUserPool();
+    var cognitoUser= userPool.getCurrentUser();
+
+    cognitoUser.setAuthenticationFlowType('CUSTOM_AUTH');
+    cognitoUser.initiateAuth(new AuthenticationDetails({
+        Username: cognitoUser.getUsername()
+    }), {
+        customChallenge: async function(challengeParameters) {
+            let asseResp;
+            try {
+                const opts = JSON.parse(challengeParameters.assertionChallenge);
+                console.log('opts', opts);
+                try {
+                    asseResp = await startAuthentication(opts);
+                } catch (error) {
+                    elemError.innerText = error;
+                    console.log(error);
+                    throw new Error(error);
+                }
+            } catch (error) {
+                elemError.innerText = error;
+                throw new Error(error);
+            }
+
+            return asseResp;
+        }
+    }, {
+        onSuccess: function (result) {
+
+        }
+
+});
+
+
+
+
+
+
+
 const credential = new PublicKeyCredential({
     id: credentialIdFromWebAuthn,
     rawId: base64url.decode(credentialIdFromWebAuthn),
