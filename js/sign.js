0e1f62a554121cf51adf0ee82e609a1197336306
Hagman01 <wille.hagman@gmail.com>
2023-04-28 12:33:18 +0200

    tog bort oanvända funktioner inom signjs filen samt docs.html

--- js/sign.js
+++ js/sign.js
@@ -91,22 +91,3 @@ console.log('Signature:', base64url.encode(signature2));*/
 
 
 
-
-function callLambda(){
-    $.ajax({
-        url: "https://8sehj03dh7.execute-api.eu-north-1.amazonaws.com/default/THESIS-secureinbrowser-documentSign/documentsign/myfunction",
-        type: "POST",
-        contentType: "application/json",
-        data: JSON.stringify({
-            key1: "hello",
-            key2: "world",
-            key3: "testing"
-        }),
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (xhr, status, error) {
-            console.log(xhr.responseText);
-        }
-    });
-}
