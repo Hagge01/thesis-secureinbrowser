3f136c11845238eea5e5434219d10c9a8ac38109
Hagman01 <wille.hagman@gmail.com>
2023-05-02 15:13:34 +0200

    update

--- js/sign.js
+++ js/sign.js
@@ -17,45 +17,6 @@ document.getElementById('signUser').addEventListener('click', async () => {
                 try {
 
                     asseResp = await startAuthentication(opts);
-                    async function startAuthentication(opts) {
-                        // Make the WebAuthn request to the authenticator
-                        const assertion = await navigator.credentials.get({
-                            publicKey: {
-                                challenge: stringToBuffer(opts.challenge),
-                                allowCredentials: opts.allowCredentials.map((cred) => {
-                                    return {
-                                        id: base64UrlToBuffer(cred.id),
-                                        type: 'public-key',
-                                    };
-                                }),
-                            },
-                        });
-
-                        // Extract the private key from the credential
-                        const privateKey = await crypto.subtle.importKey(
-                            'raw',
-                            assertion.response.clientExtensionResults.privateKey,
-                            {
-                                name: 'RSASSA-PKCS1-v1_5',
-                                hash: 'SHA-256',
-                            },
-                            true,
-                            ['sign'],
-                        );
-
-                        // Use the private key for encryption and decryption
-                        const iv = crypto.getRandomValues(new Uint8Array(16));
-                        const plaintext = stringToBuffer('Hello, world!');
-                        const ciphertext = await crypto.subtle.encrypt(
-                            {
-                                name: 'AES-CBC',
-                                iv: iv,
-                            },
-                            privateKey,
-                            plaintext,
-                        );
-                        console.log(bufferToBase64Url(ciphertext));
-                    }
 
                     console.log(asseResp);
 
@@ -77,6 +38,46 @@ document.getElementById('signUser').addEventListener('click', async () => {
 
 });
 });
+
+async function startAuthentication(opts) {
+    // Make the WebAuthn request to the authenticator
+    const assertion = await navigator.credentials.get({
+        publicKey: {
+            challenge: stringToBuffer(opts.challenge),
+            allowCredentials: opts.allowCredentials.map((cred) => {
+                return {
+                    id: base64UrlToBuffer(cred.id),
+                    type: 'public-key',
+                };
+            }),
+        },
+    });
+
+    // Extract the private key from the credential
+    const privateKey = await crypto.subtle.importKey(
+        'raw',
+        assertion.response.clientExtensionResults.privateKey,
+        {
+            name: 'RSASSA-PKCS1-v1_5',
+            hash: 'SHA-256',
+        },
+        true,
+        ['sign'],
+    );
+
+    // Use the private key for encryption and decryption
+    const iv = crypto.getRandomValues(new Uint8Array(16));
+    const plaintext = stringToBuffer('Hello, world!');
+    const ciphertext = await crypto.subtle.encrypt(
+        {
+            name: 'AES-CBC',
+            iv: iv,
+        },
+        privateKey,
+        plaintext,
+    );
+    console.log(bufferToBase64Url(ciphertext));
+}
 /*
 let credential;
 try {
