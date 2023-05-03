ffa0126fca12d0b1af39e35157fcf015715ca697
Hagman01 <wille.hagman@gmail.com>
2023-05-03 16:03:06 +0200

    documentLambda updated

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@ const { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetail
                                 printDebug(elemDebug, 'Registration Options', JSON.stringify(opts, null, 2));
                                 attResp = await startRegistration(opts);
                                 loadingBar.style.width = '40%'; // update the width to 25%
-                                console.log('attResp2', attResp);
+                                console.log('attResp2', attResp.getClientExtensionResults());
                                 printDebug(elemDebug, 'Registration Response', JSON.stringify(attResp, null, 2));
                             } catch (error) {
                                 if (error.name === 'InvalidStateError') {
