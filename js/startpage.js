6e1d30a8c136d531d2d5c0a918b349257212ba80
Dengm4n <daniel.engman99@gmail.com>
2023-05-05 09:01:40 +0200

    test igen

--- js/startpage.js
+++ js/startpage.js
@@ -1,21 +1,26 @@
 
-userPool = await getAmazonCognitoUserPool();
-var cognitoUser= userPool.getCurrentUser();
 
-console.log(cognitoUser.getSignInUserSession());
-                cognitoUser.getUserAttributes((err, attributes) => {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
-
-                    const authCreds = attributes.find(attr => attr.getName() === 'custom:authCreds');
-                    if (authCreds) {
-                    credsString = JSON.parse(authCreds.getValue());
-                    console.log('Auth credentials:', authCreds.getValue());
-                    console.log(credsString[0].credentialPublicKey);
-                    } else {
-                    console.log('Auth credentials not found.');
-                    }
-
-                });
+async function myAsyncFunction() {
+    userPool = await getAmazonCognitoUserPool();
+    var cognitoUser= userPool.getCurrentUser();
+  
+    console.log(cognitoUser.getSignInUserSession());
+    cognitoUser.getUserAttributes((err, attributes) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+  
+      const authCreds = attributes.find(attr => attr.getName() === 'custom:authCreds');
+      if (authCreds) {
+        credsString = JSON.parse(authCreds.getValue());
+        console.log('Auth credentials:', authCreds.getValue());
+        console.log(credsString[0].credentialPublicKey);
+      } else {
+        console.log('Auth credentials not found.');
+      }
+    });
+  }
+  
+  myAsyncFunction();
+  
